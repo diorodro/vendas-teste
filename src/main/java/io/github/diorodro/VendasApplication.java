1b5e5c34b7ddca721ae5bf735aae505031d2ee2e
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-06 02:45:07 -0300

    primeiro commit

--- src/main/java/io/github/diorodro/VendasApplication.java
+++ src/main/java/io/github/diorodro/VendasApplication.java
@@ -15,7 +15,6 @@ public class VendasApplication {
 	@Autowired
 	@Qualifier("applicationName")
 	private String applicationName;
-//	modificando para mudar no github
 	
 	@Value("${application.name}")
 	private String appName;
